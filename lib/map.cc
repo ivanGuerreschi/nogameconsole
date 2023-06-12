1fcb0b7e26b3281222ef95cf0892186f7fa52609
Ivan Guerreschi <ivanguerreschi86@gmail.com>
2023-06-12 16:53:38 +0200

    Move creation mine in main.cc and control limit map

--- lib/map.cc
+++ lib/map.cc
@@ -21,25 +21,10 @@
 #include "hero.h"
 #include "map.h"
 #include "mine.h"
-#include <cstdlib>
-#include <ctime>
 #include <iostream>
 
-Mine *mine1;
-Mine *mine2;
-Mine *mine3;
-Mine *mine4;
-Mine *mine5;
-
 Map::Map (char mc) : mapchar (mc)
-{
-  std::srand (static_cast<unsigned int> (std::time (NULL)));
-  mine1 = new Mine ('*', std::rand () % 10, std::rand () % 10);
-  mine2 = new Mine ('*', std::rand () % 10, std::rand () % 10);
-  mine3 = new Mine ('*', std::rand () % 10, std::rand () % 10);
-  mine4 = new Mine ('*', std::rand () % 10, std::rand () % 10);
-  mine5 = new Mine ('*', std::rand () % 10, std::rand () % 10);
-}
+{}
 
 std::vector<std::vector<char>>
 Map::make_map (int x_flag, int y_flag)
@@ -57,24 +42,7 @@ Map::print_map(std::vector<std::vector<char>> map,
 	       char hc, int hx, int hy)
 {
     std::system ("clear");
-    
-    if ((hx == mine1->get_x_mine () && hy == mine1->get_x_mine ()) ||
-	(hx == mine2->get_x_mine () && hy == mine2->get_x_mine ()) ||
-	(hx == mine3->get_x_mine () && hy == mine3->get_x_mine ()) ||
-	(hx == mine4->get_x_mine () && hy == mine4->get_x_mine ()) ||
-	(hx == mine5->get_x_mine () && hy == mine5->get_x_mine ()))
-      {
-
-	std::cout << "GAMEOVER!" << std::endl;
-	std::cout << "Pres q to exit" << std::endl;
-	hc = '*';
-	delete mine1;
-	delete mine2;
-	delete mine3;
-	delete mine4;
-	delete mine5;
-      }
-    
+     
     map[static_cast<unsigned int> (hy)][static_cast<unsigned int> (hx)] = hc;
 
     for (auto w = map.begin (); w != map.end (); w++)
--- src/main.cc
+++ src/main.cc
@@ -21,6 +21,9 @@
 #include "hero.h"
 #include "info.h"
 #include "map.h"
+#include "mine.h"
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 
 int
@@ -30,7 +33,14 @@ main ()
   Hero hero { '@' , 0, 0 };
   Info info {};
   Map map { 'O' };
-
+  
+  std::srand (static_cast<unsigned int> (std::time (NULL)));
+  Mine *mine1 = new Mine ('*', std::rand () % 10, std::rand () % 10);
+  Mine *mine2 = new Mine ('*', std::rand () % 10, std::rand () % 10);
+  Mine *mine3 = new Mine ('*', std::rand () % 10, std::rand () % 10);
+  Mine *mine4 = new Mine ('*', std::rand () % 10, std::rand () % 10);
+  Mine *mine5 = new Mine ('*', std::rand () % 10, std::rand () % 10);
+  
   std::cout << info.get_package () << std::endl;
 
   auto newmap = map.make_map (flag.get_x_flag (), flag.get_y_flag ());
@@ -52,18 +62,26 @@ main ()
           break;
 	case 'l':
           hero.set_x_hero (hero.get_x_hero () + 1);
+	  if (hero.get_x_hero () > 9)
+	    hero.set_x_hero(9);
           map.print_map (newmap, hero.get_herochar (), hero.get_x_hero (), hero.get_y_hero ());
           break;
         case 'h':
           hero.set_x_hero (hero.get_x_hero () - 1);
+	  if (hero.get_x_hero () < 0)
+	    hero.set_x_hero(0);
           map.print_map (newmap, hero.get_herochar (), hero.get_x_hero (), hero.get_y_hero ());
           break;
         case 'k':
           hero.set_y_hero (hero.get_y_hero () + 1);
+	  if (hero.get_y_hero () > 9)
+	    hero.set_y_hero(9);
           map.print_map (newmap, hero.get_herochar (), hero.get_x_hero (), hero.get_y_hero ());
           break;
         case 'j':
           hero.set_y_hero (hero.get_y_hero() - 1);
+	  if (hero.get_y_hero () < 0)
+	    hero.set_y_hero(0);
           map.print_map (newmap, hero.get_herochar (), hero.get_x_hero (), hero.get_y_hero ());
           break;
         }
@@ -71,9 +89,28 @@ main ()
       if (hero.get_x_hero () == flag.get_x_flag () && hero.get_y_hero () == flag.get_y_flag ())
         {
           std::cout << "WIN!!" << std::endl;
-          loop = false;
-        }
+	  loop = false;
+        }      
+      
+      if ((hero.get_x_hero () == mine1->get_x_mine () && hero.get_y_hero () == mine1->get_y_mine ()) ||
+	  (hero.get_x_hero () == mine2->get_x_mine () && hero.get_y_hero () == mine2->get_y_mine ()) ||
+	  (hero.get_x_hero () == mine3->get_x_mine () && hero.get_y_hero () == mine3->get_y_mine ()) ||
+	  (hero.get_x_hero () == mine4->get_x_mine () && hero.get_y_hero () == mine4->get_y_mine ()) ||
+	  (hero.get_x_hero () == mine5->get_x_mine () && hero.get_y_hero () == mine5->get_y_mine ()))
+	{
+
+	  std::cout << "GAMEOVER!!" << std::endl;
+	  hero.set_herochar ('*');
+	  loop = false;
+
+	}
     }
 
+  delete mine1;
+  delete mine2;
+  delete mine3;
+  delete mine4;
+  delete mine5;
+  
   return 0;
 }
